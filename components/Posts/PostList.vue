4f0fac0a4208db451add75464690a3791f609d57
adiputra22 <tuts.adiputra@gmail.com>
2020-07-24 22:11:34 +0700

    post list component

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class='post-list'>
+        <PostPreview 
+            id="1"
+            thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
+            title="Hello there!"
+            previewText="This is preview text bla bla bla"
+            />
+        <PostPreview 
+            id="2"
+            thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
+            title="Hello there 2!"
+            previewText="This is preview text bla bla bla"
+            />
+        <PostPreview 
+            id="2"
+            thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
+            title="Hello there 3!"
+            previewText="This is preview text bla bla bla"
+            />
+    </section>    
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+
+export default {
+    components: {
+        PostPreview
+    }    
+}
+</script>
+
+<style scoped>
+.post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,16 @@
       <h1>Get the latest tech news!</h1>
     </section>
 
-    <section class="featured-posts">
-      <PostPreview 
-        id="1"
-        thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
-        title="Hello there!"
-        previewText="This is preview text bla bla bla"
-        />
-      <PostPreview 
-        id="2"
-        thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
-        title="Hello there 2!"
-        previewText="This is preview text bla bla bla"
-        />
-      <PostPreview 
-        id="2"
-        thumbnail="https://images.pexels.com/photos/2092872/pexels-photo-2092872.jpeg"
-        title="Hello there 3!"
-        previewText="This is preview text bla bla bla"
-        />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview';
+import PostList from '@/components/Posts/PostList';
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
@@ -67,14 +48,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
